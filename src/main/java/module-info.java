cf177a38b23a3c15f2b9f5121516ea80ab472681
Eddy Mora P <filibusterocanfi@gmail.com>
2026-02-10 17:15:28 -0600

    Crear persistencia de datos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.proyectoprogramado2026 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens com.mycompany.proyectoprogramado2026 to javafx.fxml;
     opens controllers to javafx.fxml;
